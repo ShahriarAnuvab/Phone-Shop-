3042261b8892e38c5fbeb8ff5bb60f3fb5da06e7
ShahriarAnuvab <shahriarrahmananuvab@gmail.com>
2023-09-29 03:38:39 +0600

    commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,8 +14,9 @@ const Home = () => {
 
   // Filter phones based on search input
   const filteredPhones = phones.filter((phone) =>
-    phone.phone_name.toLowerCase().includes(searchInput.toLowerCase())
+    phone.brand_name.toLowerCase().includes(searchInput.toLowerCase())
   );
+  
 
   return (
     <div>
@@ -26,13 +27,25 @@ const Home = () => {
         />
       </div>
       <div>
-        <h1 className="text-3xl font-semibold text-center my-5">Our Phone Collections</h1>
-        <h1 className="text-2xl font-medium text-center my-5">Total Phone: {phones.length}</h1>
+        <h1 className="text-3xl font-semibold text-center my-5">
+          Our Phone Collections
+        </h1>
+        <h1 className="text-2xl font-medium text-center my-5">
+          Total Phone: {filteredPhones.length}
+        </h1>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-5">
-        {filteredPhones.map((phone) => (
-          <Phones key={phone.id} phone={phone} />
-        ))}
+        {filteredPhones.length === 0 ? (
+          <div className="text-center w-full   md:ml-[180px] lg:ml-80 xl:ml-[440px]">
+            <p className="text-3xl font-bold text-pink-600 text-center">
+              No Match
+            </p>
+          </div>
+        ) : (
+          filteredPhones?.map((phone) => (
+            <Phones key={phone.id} phone={phone} />
+          ))
+        )}
       </div>
     </div>
   );
